546dd9954b850f3b56458890ea71d15c32e60ed7
naveenk4250-code <naveenk4250@gmail.com>
2026-02-25 18:22:39 +0530

    Create SumOfEvenNumbers.java

---
+++ module-1/SumOfEvenNumbers.java
@@ -0,0 +1,11 @@
+public class SumOfEvenNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 2;
+        while (i <= 100) {
+            sum += i;
+            i += 2;
+        }
+        System.out.println("Sum of all even numbers between 1 and 100 is: " + sum);
+    }
+}
