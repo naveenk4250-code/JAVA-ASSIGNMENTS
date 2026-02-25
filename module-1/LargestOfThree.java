604881f32c5aa2b20effd930d5f77186249215f7
naveenk4250-code <naveenk4250@gmail.com>
2026-02-25 18:21:31 +0530

    Create LargestOfThree.java

---
+++ module-1/LargestOfThree.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        System.out.print("Enter third number: ");
+        int c = scanner.nextInt();
+        if (a >= b && a >= c) {
+            System.out.println(a + " is the largest number");
+        } else if (b >= a && b >= c) {
+            System.out.println(b + " is the largest number");
+        } else {
+            System.out.println(c + " is the largest number");
+        }
+        scanner.close();
+    }
+}
