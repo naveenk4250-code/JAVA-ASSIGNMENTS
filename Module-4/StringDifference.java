d31daef0243717a0dae6dcee35f72e71472fa338
naveenk4250-code <naveenk4250@gmail.com>
2026-03-03 20:42:10 +0530

    Create StringDifference.java

---
+++ Module-4/StringDifference.java
@@ -0,0 +1,15 @@
+public class StringDifference {
+    public static void main(String[] args) {
+        String s = "Hello";
+        s.concat(" World");
+        System.out.println("String: " + s);
+
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" World");
+        System.out.println("StringBuilder: " + sb);
+
+        StringBuffer sbf = new StringBuffer("Hello");
+        sbf.append(" World");
+        System.out.println("StringBuffer: " + sbf);
+    }
+}
