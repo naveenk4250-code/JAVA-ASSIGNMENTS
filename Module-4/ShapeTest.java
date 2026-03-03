6858c287188304bf5a7fd6b24e730e07753a1f13
naveenk4250-code <naveenk4250@gmail.com>
2026-03-03 20:41:35 +0530

    Create ShapeTest.java

---
+++ Module-4/ShapeTest.java
@@ -0,0 +1,24 @@
+abstract class Shape {
+    abstract double area();
+}
+
+class Circle extends Shape {
+    double radius;
+    Circle(double r) { radius = r; }
+    double area() { return 3.14 * radius * radius; }
+}
+
+class Rectangle extends Shape {
+    double length, width;
+    Rectangle(double l, double w) { length = l; width = w; }
+    double area() { return length * width; }
+}
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape s1 = new Circle(5);
+        Shape s2 = new Rectangle(4, 6);
+        System.out.println("Circle Area: " + s1.area());
+        System.out.println("Rectangle Area: " + s2.area());
+    }
+}
