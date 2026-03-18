aba52140c2e72944289d7324f35e7093e657dbbf
naveenk4250-code <naveenk4250@gmail.com>
2026-03-18 20:31:09 +0530

    Create AddTwoNumbers.java

---
+++ Module-10/AddTwoNumbers.java
@@ -0,0 +1,40 @@
+// LeetCode 2 - Add Two Numbers (Medium)
+
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+
+    private static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        AddTwoNumbers at = new AddTwoNumbers();
+        ListNode result = at.addTwoNumbers(l1, l2);
+        printList(result);
+    }
+}
