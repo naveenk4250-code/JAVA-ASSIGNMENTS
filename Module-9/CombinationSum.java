40639620105c1a57542d8c94f6cb69744d944f02
naveenk4250-code <naveenk4250@gmail.com>
2026-03-18 20:29:03 +0530

    Create CombinationSum.java

---
+++ Module-9/CombinationSum.java
@@ -0,0 +1,31 @@
+// LeetCode 39 - Combination Sum (Medium)
+
+import java.util.*;
+
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] candidates, int remain, int start) {
+        if (remain < 0) return;
+        if (remain == 0) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            temp.add(candidates[i]);
+            backtrack(result, temp, candidates, remain - candidates[i], i);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        CombinationSum cs = new CombinationSum();
+        int[] candidates = {2, 3, 6, 7};
+        int target = 7;
+        System.out.println(cs.combinationSum(candidates, target));
+    }
+}
