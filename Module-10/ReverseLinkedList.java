784c62264e374d71920fa1264933bebe3e9cb5a5
naveenk4250-code <naveenk4250@gmail.com>
2026-03-18 20:30:08 +0530

    Create ReverseLinkedList.java

---
+++ Module-10/ReverseLinkedList.java
@@ -0,0 +1,38 @@
+// LeetCode 206 - Reverse Linked List (Easy)
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    private static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ReverseLinkedList rl = new ReverseLinkedList();
+        ListNode reversed = rl.reverseList(head);
+        printList(reversed);
+    }
+}
