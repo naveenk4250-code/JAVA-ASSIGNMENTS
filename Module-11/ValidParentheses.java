b82eafb09c80ee412268f4a5b9bcf60d70e1051d
naveenk4250-code <naveenk4250@gmail.com>
2026-03-18 20:31:38 +0530

    Create ValidParentheses.java

---
+++ Module-11/ValidParentheses.java
@@ -0,0 +1,27 @@
+// LeetCode 20 - Valid Parentheses (Easy)
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        ValidParentheses vp = new ValidParentheses();
+        System.out.println(vp.isValid("()[]{}"));
+        System.out.println(vp.isValid("([)]"));
+    }
+}
